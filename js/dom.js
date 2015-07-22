f6f2ea1119b819c0a37bd58facac3af8f85e6d15
brandon932 <bthornton_932@yahoo.com>
2015-07-22 17:47:19 -0600

    Refactored code and cleaned it up

--- js/dom.js
+++ js/dom.js
@@ -1,35 +1,3 @@
-/*
-
-Hints for the first problem:
-
-  1. Find the button with the ID of `sum-all`
-  2. Add an event listener that:
-    - finds the select element by name (use getElementsByName)
-    - passes all of its options to the `sum` function defined in `logic.js`
-    - takes the value returned by the `sum` function and displays it on the page
-
-*/
-// Function to get all values within the list named "prices"
-function allValues(){
-var options = document.getElementsByName("prices")[0];
-var values = [];
-  for (var i = 0; i < options.length; i++) {
-    // values = values.push(options[i].value);
-    values.push(options[i].value);
-  }
-  return values;
-}
-
-function selectedValues(){
-var options = document.getElementsByName("prices")[0];
-var   values = [];
-  for (var i = 0; i < options.length; i++) {
-    if (options[i].selected === true){
-    values.push(options[i].value);
-    }
-  }
-  return values;
-}
 
 function sumAll(){
    answer.innerHTML = sum(allValues());
@@ -46,7 +14,6 @@ function averageAll(){
 function averageSelected(){
   answer.innerHTML = average(selectedValues());
 }
-
 
 window.onload = function(){
  var sumAllButton = document.getElementById("sum-all");
@@ -70,11 +37,4 @@ window.onload = function(){
   averageSelectedButton.addEventListener("click", function(){
     averageSelected();
   });
-
-
-
 };
-
-
-//document.getElementById("sum-all").addEventListener("click", function(){console.log("hello")})
-//sumAllButton.addEventListener("click", function(){document.getElementById("answer").innerHTML = sum(allValues)})
--- js/logic.js
+++ js/logic.js
@@ -6,34 +6,47 @@ Hints for the first problem:
     - is assigned to a variable named `sum`
     - has one parameter named `options`
     - returns the calculated sum of all of the values of all of the options passed in
-
-NOTE: use a `for` loop to make this happen.
-
 */
+
 var array1 = [1,2,3,4,0];
 function sum(options){
   var sumOf =0;
   for (var i = 0; i < options.length; i++) {
     sumOf += +options[i];
-
   }
-    // console.log(sumOf);
     return sumOf;
 }
-// sumAll(array1);
 
 function average(options){
   var numAverage = 0;
-  // for (var i = 0; i < options.length; i++) {
     x = sum(options);
     numAverage = x / options.length;
-  // }
     return numAverage;
 }
 
 function count(options){
   selectedArray = options.length;
-  // console.log(selectedArray);
   return selectedArray;
+}
 
+// Function to get all values within the list named "prices"
+function allValues(){
+var options = document.getElementsByName("prices")[0];
+var values = [];
+  for (var i = 0; i < options.length; i++) {
+    // values = values.push(options[i].value);
+    values.push(options[i].value);
+  }
+  return values;
+}
+
+function selectedValues(){
+var options = document.getElementsByName("prices")[0];
+var   values = [];
+  for (var i = 0; i < options.length; i++) {
+    if (options[i].selected === true){
+    values.push(options[i].value);
+    }
+  }
+  return values;
 }
